0f3e6907e25900d7a813aff6be2cf0cbd636f400
Calvin Loncaric <marvinx03@gmail.com>
2021-05-31 11:53:15 -0700

    Add an example of how to append to a Bash array

--- wisdom/bash-arrays.sh
+++ wisdom/bash-arrays.sh
@@ -6,3 +6,7 @@ arr=('x' 'y')
 echo "${arr[0]}" # 'x'
 echo "${arr[2]}" # ''
 echo "${arr[@]}" # equivalent to `echo 'x' 'y'`
+
+# Append an element / concatenate another array
+arr+=('z')
+echo "${arr[@]}" # 'x y z'
