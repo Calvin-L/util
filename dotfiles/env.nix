c0c1a6c071a70477cb2fd5fdf599b9f985d5a585
Calvin Loncaric <marvinx03@gmail.com>
2024-03-28 21:45:42 -0700

    Add a few more things to Nix home environment

--- dotfiles/env.nix
+++ dotfiles/env.nix
@@ -16,6 +16,10 @@
 #       ]
 
 let nixpkgs = import <nixpkgs> {}; in
+let nixjars = (import (builtins.fetchTarball {
+  url = "https://github.com/Calvin-L/nixjars/archive/adbc4e3ff414009b54b7616c00d7781f3e9cb548.tar.gz";
+  sha256 = "1jklkdky2d4y42ljd0nj5la1x6al92zm39sk9h367zrazn8140lc";
+}) { nixpkgs=nixpkgs; }); in
 with nixpkgs;
 
 let
@@ -38,17 +42,21 @@ in
 
   # ---- Core packages
   # nix-tree
-  (python3.withPackages (p: [p.mypy p.virtualenv]))
+  (python3.withPackages (p: [p.mypy p.virtualenv p.z3]))
+  # z3 # apparently bin included in Python package???
   jq.bin
   # jdk17
   htop
   tree
   rlwrap
   pv
+  cmake
+  watch
+  nixjars.tlatools
 
   # ---- Things I do not want GC'd (but also do not want in my env)
   # `bashInteractive` is a good choice because `nix-shell` always wants it
   (pin "bashInteractive" bashInteractive)
 ]
 
-++ lib.optionals (builtins.pathExists ./local.nix) (import ./local.nix { nixpkgs=nixpkgs; pin=pin; })
+++ lib.optionals (builtins.pathExists ./local.nix) (import ./local.nix { nixpkgs=nixpkgs; nixjars=nixjars; pin=pin; })
