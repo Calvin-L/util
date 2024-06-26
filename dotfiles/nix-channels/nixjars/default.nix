6775a8ac3f0070c8e744b4ec862f4035979ec2f3
Calvin Loncaric <marvinx03@gmail.com>
2024-06-26 13:37:47 -0700

    Fix nixjars hash

--- dotfiles/nix-channels/nixjars/default.nix
+++ dotfiles/nix-channels/nixjars/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
   url = "https://github.com/Calvin-L/nixjars/archive/62936862afe1e5676ff6b027d04f9fb725052517.tar.gz";
-  sha256 = "00mi29a1r8jk1w3l03m11xifii0bb3vkvbjcs65vii2k6bxjnlsz";
+  sha256 = "05z8a3gwjkwga62z5b8y4ahgmfi35m0mmgmgy2gb0l4hwh99z5m0";
 })
