6aef80d480d6d66eaee5ef18b297082782071775
Calvin Loncaric <marvinx03@gmail.com>
2024-06-29 13:07:29 -0700

    Fix nixpkgs URL

--- dotfiles/nix-channels/nixpkgs/default.nix
+++ dotfiles/nix-channels/nixpkgs/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/Calvin-L/nixjars/archive/abd6d48f8c77.tar.gz";
+  url = "https://github.com/NixOS/nixpkgs/archive/abd6d48f8c77.tar.gz";
   sha256 = "0lisfh5b3dgcjb083nsxmffvzxzs6ir1jfsxfaf29gjpjnv7sm7f";
 })
