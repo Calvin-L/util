f2ca95b37f45cf74aedc4149f9b405faa7d9edd9
Calvin Loncaric <marvinx03@gmail.com>
2024-07-03 20:00:55 -0700

    Upgrade nixpkgs to latest unstable

--- dotfiles/nix-channels/nixpkgs/default.nix
+++ dotfiles/nix-channels/nixpkgs/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/abd6d48f8c77.tar.gz";
-  sha256 = "0lisfh5b3dgcjb083nsxmffvzxzs6ir1jfsxfaf29gjpjnv7sm7f";
+  url = "https://github.com/NixOS/nixpkgs/archive/0aeab749216e4c073cece5d34bc01b79e717c3e0.tar.gz";
+  sha256 = "0fnw58pxb9l2iskrcr7d03vkbzf19qqb23r7w0ds2a62wclzxc6h";
 })
