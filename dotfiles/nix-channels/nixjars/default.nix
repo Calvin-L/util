250dd02aaddd1dc4d796cc0d6d917a9f83919f0f
Calvin Loncaric <marvinx03@gmail.com>
2024-07-03 20:00:37 -0700

    Upgrade nixjars for Jakarta servlet-api fix

--- dotfiles/nix-channels/nixjars/default.nix
+++ dotfiles/nix-channels/nixjars/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/Calvin-L/nixjars/archive/62936862afe1e5676ff6b027d04f9fb725052517.tar.gz";
-  sha256 = "05z8a3gwjkwga62z5b8y4ahgmfi35m0mmgmgy2gb0l4hwh99z5m0";
+  url = "https://github.com/Calvin-L/nixjars/archive/36f19d336e00d45c8f2636b4938b962277c02c2d.tar.gz";
+  sha256 = "0l5n1iky7nl6gqfbksfkvp24lmi9c1l81ci7lpyspa2fffp50blv";
 })
