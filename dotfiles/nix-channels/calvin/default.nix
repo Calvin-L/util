8ea28d3fd316273c8a2833c9461bcc64c0f6dd3d
Calvin Loncaric <marvinx03@gmail.com>
2024-08-08 22:45:59 -0700

    Upgrade Nix packages

--- dotfiles/nix-channels/calvin/default.nix
+++ dotfiles/nix-channels/calvin/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/Calvin-L/calvins-nix-packages/archive/a4c48185dae3802d53576eb728a88d9393fe7a2f.tar.gz";
-  sha256 = "0nyw0pr41a0ln17yp01d5rhsfnbzb6m81f3mdj718qc87z50g7bs";
+  url = "https://github.com/Calvin-L/calvins-nix-packages/archive/ebd6a5edfcdc803e407b51a01b4460360cf52ae6.tar.gz";
+  sha256 = "103n9pq13akm23qaywzgqlwwz65qsc5p0dyqbgvhj736zx0wp8jb";
 })
