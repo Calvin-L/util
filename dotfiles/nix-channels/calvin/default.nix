b057c501c24bc05c00061573b47016440a0aa249
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 14:53:16 -0700

    Update my nix channel for recent fixes

--- dotfiles/nix-channels/calvin/default.nix
+++ dotfiles/nix-channels/calvin/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/Calvin-L/calvins-nix-packages/archive/f4a81a581bf5666dcafbd2c8ff6d0bc0e388b40f.tar.gz";
-  sha256 = "0s8gji0qg3js95cxhw3d7n40np3a152js0xd4j4xhha2a2hdij2c";
+  url = "https://github.com/Calvin-L/calvins-nix-packages/archive/1b939ee35ece391f0bd65b084f1189c968b34d06.tar.gz";
+  sha256 = "0dwp5vdqq1w7s291nbfs63rpmmwqxw25fcyq950rk494x06amk6n";
 })
