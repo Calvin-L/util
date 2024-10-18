139848c42c895b1d53e643bef65482fa86d3ac68
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 22:25:06 -0700

    Upgrade nixjars

--- dotfiles/nix-channels/nixjars/default.nix
+++ dotfiles/nix-channels/nixjars/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/Calvin-L/nixjars/archive/78710548665da11cf69d08fa4586dead4dda0695.tar.gz";
-  sha256 = "0i7nv07a34kh801kff15av8c4rpdydw02v23la7wf1hbz7aw1kx4";
+  url = "https://github.com/Calvin-L/nixjars/archive/3c26d1f5a183bb8be5318f911e4c1d1ea1183665.tar.gz";
+  sha256 = "1pfib50arvif47xa3djs2c58ijknqd55mp4pcv5gjrqkfqqckvr0";
 })
