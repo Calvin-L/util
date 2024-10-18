d2c5b20bf761caa78b1fe94ed5fbf2f7112a92a0
Calvin Loncaric <marvinx03@gmail.com>
2024-10-17 19:46:41 -0700

    Upgrade nixpkgs to latest unstable

--- dotfiles/nix-channels/nixpkgs/default.nix
+++ dotfiles/nix-channels/nixpkgs/default.nix
@@ -1,6 +1,6 @@
 let src = builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/0aeab749216e4c073cece5d34bc01b79e717c3e0.tar.gz";
-  sha256 = "0fnw58pxb9l2iskrcr7d03vkbzf19qqb23r7w0ds2a62wclzxc6h";
+  url = "https://github.com/NixOS/nixpkgs/archive/7881fbfd2e3ed1dfa315fca889b2cfd94be39337.tar.gz";
+  sha256 = "0na5zykmva0a6valzrrcigp6g0rzq28mi7dqxqr0s3jbn6fm24hq";
 }; in
 args: (import src args).appendOverlays [
   # NOTE 2024/10/17: need coq_8_19 because CoqHamer isn't 8.20 compatible, see
