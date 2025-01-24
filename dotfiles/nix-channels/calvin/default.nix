ba19e30b5c7657415aeb36a57258e8fe9476e632
Calvin Loncaric <marvinx03@gmail.com>
2025-01-24 10:51:11 -0800

    Upgrade my nix channel for latest CalTac

--- dotfiles/nix-channels/calvin/default.nix
+++ dotfiles/nix-channels/calvin/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/Calvin-L/calvins-nix-packages/archive/745d9fca90006c8bfc54bb7b2f3e2a895f9aba13.tar.gz";
-  sha256 = "08bcn8ga4s4hmrq4shmknkcz2jqippvd5y9g59072p3jk2p0qpig";
+  url = "https://github.com/Calvin-L/calvins-nix-packages/archive/bff7ab96a4399e74ca48cfa64579028c636d766d.tar.gz";
+  sha256 = "1q6zj0wjbqvi7mxi7iq6j39gvykqvm5a97l4xwz0p1q6jng0skia";
 })
