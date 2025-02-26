37b86806cbd4d9bfd24f9dfdd1b33d6b5a4931f9
Calvin Loncaric <marvinx03@gmail.com>
2025-02-25 19:24:42 -0800

    Update my nix repo for TLAPS fixes

--- dotfiles/nix-channels/calvin/default.nix
+++ dotfiles/nix-channels/calvin/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/Calvin-L/calvins-nix-packages/archive/5ee377eed66a3145676cd586da53211475fe8849.tar.gz";
-  sha256 = "0n5wc86xy7w3zm8z40gliwxfp366kdvakdrrwgsb767krbc686c0";
+  url = "https://github.com/Calvin-L/calvins-nix-packages/archive/757a1ed9a362c92194921ef2c9d03d89f453cf8c.tar.gz";
+  sha256 = "0xdhh8as6yd4d2qvc45mxfk064cwxizwk6j1xmghna6zhy7z51g6";
 })
