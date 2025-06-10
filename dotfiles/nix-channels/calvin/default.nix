9ea34258f598adca5f28ca667d9e689489419114
Calvin Loncaric <marvinx03@gmail.com>
2025-06-09 22:25:37 -0700

    Upgrade repo for latest Apalache

--- dotfiles/nix-channels/calvin/default.nix
+++ dotfiles/nix-channels/calvin/default.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/Calvin-L/calvins-nix-packages/archive/dc5c0712d8982aed3e905737bf40d38dc2c83f49.tar.gz";
-  sha256 = "1xhk4x8d96qbyiv4gshq52k2iqhnm0a1pna9h0pc28shkjc2mr6q";
+  url = "https://github.com/Calvin-L/calvins-nix-packages/archive/3381800a88b37e4a96306fb115667c490730c42c.tar.gz";
+  sha256 = "19clc7kkr0ksyyzana1pf2l5bg26hnc0ksiiimzwmf0qww8m0rrh";
 })
